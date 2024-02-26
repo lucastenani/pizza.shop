d6cf99eaa3e603064e38086d8741e9f4e021a77d
lucastenani <lucas.tenani3@gmail.com>
2024-02-26 17:42:35 -0300

    Style login component

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,10 +1,19 @@
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div>
-      <header>auth header</header>
-      <main>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <header className="flex items-center gap-3 text-lg text-foreground">
+          <Pizza className="h-5 w-5" />
+          <h2 className="font-semibold">pizza.shop</h2>
+        </header>
+        <footer className="text-sm">
+          Partner Dashboard &copy; pizza.shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+      <main className="flex flex-col items-center justify-center">
         <Outlet />
       </main>
     </div>
