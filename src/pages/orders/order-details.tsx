fbbc0db929ef565632604246108b2b472a008359
lucastenani <lucas.tenani3@gmail.com>
2024-02-27 17:35:15 -0300

    Create Order Details Component

---
+++ src/pages/orders/order-details.tsx
@@ -0,0 +1,97 @@
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+export function OrderDetails() {
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Order: ada9s8das90d8</DialogTitle>
+        <DialogDescription>Order details</DialogDescription>
+      </DialogHeader>
+
+      <div className="space-y-6">
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Status</TableCell>
+              <TableCell className="flex justify-end">
+                <div className="flex items-center gap-2">
+                  <span className="h-2 w-2 rounded-full bg-slate-400" />
+                  <span className="font-medium text-muted-foreground">
+                    Pending
+                  </span>
+                </div>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Client</TableCell>
+              <TableCell className="flex justify-end">Lucas Tenani</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Phone</TableCell>
+              <TableCell className="flex justify-end">(555) 123-4567</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="text-muted-foreground">Email</TableCell>
+              <TableCell className="flex justify-end">
+                lucas.tenani@gmail.com
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="text-muted-foreground">
+                Realized ago
+              </TableCell>
+              <TableCell className="flex justify-end">15 minutes</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Product</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+              <TableHead className="text-right">Price</TableHead>
+              <TableHead className="text-right">Subtotal</TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            <TableRow>
+              <TableCell>Pepperoni pizza</TableCell>
+              <TableCell className="text-right">2</TableCell>
+              <TableCell className="text-right">$15.99</TableCell>
+              <TableCell className="text-right">$31.98</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Tuscan pizza</TableCell>
+              <TableCell className="text-right">1</TableCell>
+              <TableCell className="text-right">$12.99</TableCell>
+              <TableCell className="text-right">$12.99</TableCell>
+            </TableRow>
+          </TableBody>
+
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={3}>Total order</TableCell>
+              <TableCell className="text-right font-medium">$44.97</TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </div>
+    </DialogContent>
+  )
+}
