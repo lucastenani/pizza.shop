fa42872fdb1f72447fed20a86236dca684adf5ef
lucastenani <lucas.tenani3@gmail.com>
2024-03-09 19:23:38 -0300

    Leave the filters functional

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -1,14 +1,18 @@
+import { OrderStatus } from '@/components/order-status'
 import { api } from '@/lib/axios'
 
 export interface GetOrdersQuery {
   pageIndex?: number | null
+  orderId?: string | null
+  customerName?: string | null
+  status?: string | null
 }
 
 export interface GetOrdersResponse {
   orders: {
     orderId: string
     createdAt: string
-    status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+    status: OrderStatus
     customerName: string
     total: number
   }[]
@@ -19,10 +23,18 @@ export interface GetOrdersResponse {
   }
 }
 
-export async function getOrders({ pageIndex }: GetOrdersQuery) {
+export async function getOrders({
+  pageIndex,
+  customerName,
+  orderId,
+  status,
+}: GetOrdersQuery) {
   const response = await api.get<GetOrdersResponse>('/orders', {
     params: {
       pageIndex,
+      orderId,
+      customerName,
+      status,
     },
   })
 
